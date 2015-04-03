52d417ba292bb2a596d51208910abe6019da5910
David Zulaica <zulaica@gmail.com>
2015-04-03 15:03:10 -0700

    Set up comment posting to use AJAX.

--- spec/features/create_new_comment_pages_spec.rb
+++ spec/features/create_new_comment_pages_spec.rb
@@ -17,9 +17,17 @@ describe "the create new comment process" do
   end
 
   it "throws an error when the form isn't filled out" do
-    visit post_path(post)
     visit new_post_comment_path(post)
     click_on "Create Comment"
     expect(page).to have_content "errors"
+  end
+
+  it "creates a new comment on a post through AJAX", js: true do
+    visit post_path(post)
+    click_on "Add a comment"
+    expect(page).to have_selector(:link_or_button, "Create Comment")
+    fill_in "Text", :with => "Comment text."
+    click_on "Create Comment"
+    expect(page).not_to have_content "There are no comments yet!"
   end
 end
